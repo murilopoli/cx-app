0e6ad7a55c8ae7bed8df759684d533b13a6ab364
Murilo Poli <76956502+murilopoli@users.noreply.github.com>
2024-06-04 21:52:43 +0000

    modified:   pages/api/checkout_sessions.js

--- pages/api/checkout_sessions.js
+++ pages/api/checkout_sessions.js
@@ -1,4 +1,5 @@
 // pages/api/checkout_sessions.js
+import { stripe } from '@stripe/stripe-js'
 const stripe = require('stripe')(process.env.STRIPE_SECRET_KEY)
 
 export default async function handler(req, res) {
